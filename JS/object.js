400ef2df1e41d705ac9bf9b415cd5db5e9b34f73
aron7 <aron@beeldr.nl>
2024-11-26 16:02:06 +0100

    made core and core points invisible with comment

--- JS/object.js
+++ JS/object.js
@@ -19,58 +19,58 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('enemyAttackScreen').appendChild(player);
 
-    let playerCore = document.createElement('div');
-    playerCore.id = "playerCore";
-    playerCore.style.width = '10px';
-    playerCore.style.height = '10px';
-    playerCore.style.backgroundColor = 'purple';
-    playerCore.style.display = "flex";
-    playerCore.style.alignItems = "center";
-    playerCore.style.justifyContent = "center";
+    // let playerCore = document.createElement('div');
+    // playerCore.id = "playerCore";
+    // playerCore.style.width = '10px';
+    // playerCore.style.height = '10px';
+    // playerCore.style.backgroundColor = 'purple';
+    // playerCore.style.display = "flex";
+    // playerCore.style.alignItems = "center";
+    // playerCore.style.justifyContent = "center";
 
-    player.appendChild(playerCore);
+    // player.appendChild(playerCore);
 
     //these are only for visible hitbox refrence can remove when not want to see the hitbox
 
-    let playerCorePoint1 = document.createElement('div');
-    playerCorePoint1.id = "playerCorePoint1";
-    playerCorePoint1.style.width = '5px';
-    playerCorePoint1.style.height = '5px';
-    playerCorePoint1.style.backgroundColor = 'pink';
-    playerCorePoint1.style.position = "absolute";
-    playerCorePoint1.style.bottom = '25px'
+    // let playerCorePoint1 = document.createElement('div');
+    // playerCorePoint1.id = "playerCorePoint1";
+    // playerCorePoint1.style.width = '5px';
+    // playerCorePoint1.style.height = '5px';
+    // playerCorePoint1.style.backgroundColor = 'pink';
+    // playerCorePoint1.style.position = "absolute";
+    // playerCorePoint1.style.bottom = '25px'
 
-    playerCore.appendChild(playerCorePoint1);
+    // playerCore.appendChild(playerCorePoint1);
 
-    let playerCorePoint2 = document.createElement('div');
-    playerCorePoint2.id = "playerCorePoint2";
-    playerCorePoint2.style.width = '5px';
-    playerCorePoint2.style.height = '5px';
-    playerCorePoint2.style.backgroundColor = 'pink';
-    playerCorePoint2.style.position = "absolute";
-    playerCorePoint2.style.top = '25px'
+    // let playerCorePoint2 = document.createElement('div');
+    // playerCorePoint2.id = "playerCorePoint2";
+    // playerCorePoint2.style.width = '5px';
+    // playerCorePoint2.style.height = '5px';
+    // playerCorePoint2.style.backgroundColor = 'pink';
+    // playerCorePoint2.style.position = "absolute";
+    // playerCorePoint2.style.top = '25px'
 
-    playerCore.appendChild(playerCorePoint2);
+    // playerCore.appendChild(playerCorePoint2);
 
-    let playerCorePoint3 = document.createElement('div');
-    playerCorePoint3.id = "playerCorePoint3";
-    playerCorePoint3.style.width = '5px';
-    playerCorePoint3.style.height = '5px';
-    playerCorePoint3.style.backgroundColor = 'pink';
-    playerCorePoint3.style.position = "absolute";
-    playerCorePoint3.style.left = '25px'
+    // let playerCorePoint3 = document.createElement('div');
+    // playerCorePoint3.id = "playerCorePoint3";
+    // playerCorePoint3.style.width = '5px';
+    // playerCorePoint3.style.height = '5px';
+    // playerCorePoint3.style.backgroundColor = 'pink';
+    // playerCorePoint3.style.position = "absolute";
+    // playerCorePoint3.style.left = '25px'
 
-    playerCore.appendChild(playerCorePoint3);
+    // playerCore.appendChild(playerCorePoint3);
 
-    let playerCorePoint4 = document.createElement('div');
-    playerCorePoint4.id = "playerCorePoint4";
-    playerCorePoint4.style.width = '5px';
-    playerCorePoint4.style.height = '5px';
-    playerCorePoint4.style.backgroundColor = 'pink';
-    playerCorePoint4.style.position = "absolute";
-    playerCorePoint4.style.right = '25px'
+    // let playerCorePoint4 = document.createElement('div');
+    // playerCorePoint4.id = "playerCorePoint4";
+    // playerCorePoint4.style.width = '5px';
+    // playerCorePoint4.style.height = '5px';
+    // playerCorePoint4.style.backgroundColor = 'pink';
+    // playerCorePoint4.style.position = "absolute";
+    // playerCorePoint4.style.right = '25px'
 
-    playerCore.appendChild(playerCorePoint4);
+    // playerCore.appendChild(playerCorePoint4);
 
     //these are only for visible hitbox refrence can remove when not want to see the hitbox
 
