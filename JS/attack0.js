c06e45351ffac88c2ed2619d73d51d0cf00bf001
SteveJaws <vanduijnaronzakelijk@gmail.com>
2024-11-27 20:49:44 +0100

    removed the enemy thing of the game

--- JS/attack0.js
+++ JS/attack0.js
@@ -5,23 +5,10 @@ let attack = false;
 
 function attack0(){
     attack = true;
-    let enemies = []; // this is for spawning lots of goons
-    for(let i = 0; i < 10; i++){
-        let enemy = new Enemy(Math.floor(Math.random() * (scherm.clientWidth - 150) + 150), Math.floor(Math.random() * (scherm.clientWidth - 150) + 150), Math.random() * 2.1);
-        enemy.create();
-        enemy.checkForPlayer();
-        enemy.follow();
-
-        enemies.push(enemy);
-    }
 
     arrowSpawnSystem();
 
     setTimeout(() => { // timer to end the attack
-        enemies.forEach((enemy) =>{
-            enemy.enemy.remove();
-            enemy.remove();
-        })
         menu();
         attack = false;
     },20000) //length of attack
@@ -52,7 +39,7 @@ function arrowSpawnSystem(){
         setTimeout(() => {
 
             arrowChecker = false;
-        },1000) // these are the seconds that it takes to spawn a new arrow
+        },500) // these are the seconds that it takes to spawn a new arrow
     }
     arrowRepeater();   
 }
